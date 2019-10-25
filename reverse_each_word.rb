2e242e5a104b6421777bbe06f44967fa65eac94d
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 07:51:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(mystring)
   myarray = mystring.split(' ')
  #myarray2 = []
  #str = ""
-  myarray2 =  myarray.reverse_each {|i|  "#{i}"}#.join(" ")
+  myarray2 push(myarray.reverse_each {|i|  "#{i}"}      #.join(" ")
  #str = myarray2.join(', ')
- #str
+ myarray2.join(" ")
 end
