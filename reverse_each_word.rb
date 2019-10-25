2a9f21894bc5d736349508157ebf3ef0df2819fb
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 07:14:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(mysentence)
-  myarray = %w[mysentence]
+def reverse_each_word(mystring)
+  %w myarray[0] = mystring
   str = ""
   array.reverse_each {|n| p str += "#{n} " }
 
